e459b51b8913826204dc180940fba9f9a4465c2c
Carlos Perches <perc8517@bears.unco.edu>
2024-09-02 06:42:48 -0600

    Finshed Chapter 1

--- Chapter1/BMI.js
+++ Chapter1/BMI.js
@@ -1,1 +1,23 @@
-console.log("HelloWorld!")
+const prompt = require('prompt-sync')();
+
+function BMIcalc(weight, height){
+    return weight/(height**2);
+}
+
+var weight = prompt("Enter your weight in kilograms: ");
+var height = prompt("Enter your height in meters: ");
+var BMI = BMIcalc(weight, height);
+
+if (BMI >= 30) {
+    console.log(`Your BMI is ${BMI} - Obese`);
+} 
+else if (BMI >= 25) {
+    console.log(`Your BMI is ${BMI} - Overweight`);
+} 
+else if (BMI >= 18.5) {
+    console.log(`Your BMI is ${BMI} - Normal weight`);
+} 
+else {
+    console.log(`Your BMI is ${BMI} - Underweight`);
+}
+
